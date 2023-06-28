75946542c198ddacc4a57a9f929d3aebd12fa55e
Akintademuyiwa24 <harkintademuyiwa@gmail.com>
2023-06-28 17:18:24 +0100

    fix bugs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 
-import Footer from './components/Footer';
 import { Routes, Route } from 'react-router-dom';
 import Heropage from './components/Heropage';
 import Homepage from './components/Homepage';
