45cc3bd0e7d7cf0417af7f7e5b4d32edc30371d9
EmyraldCorp <jesusboy1252@gmail.com>
2023-06-29 14:35:18 -0700

    changes to whole components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+
 import React from 'react';
 import './App.css';
 
+import Footer from './components/Footer';
 import { Routes, Route } from 'react-router-dom';
 import Heropage from './components/Heropage';
 import Homepage from './components/Homepage';
@@ -9,23 +11,25 @@ import Menu from './components/Menu';
 import Reservation from './components/Reservation';
 import OrderOnline from './components/Orderonline';
 import Login from './components/Login';
-import logo from './images/115168_45900_7388b824-b2f0-4ffa-ae56-58d13a637de8.jpg'
+import logo from '../src/images/logoo.png'
 
 function App() {
 
   return (
     <div className='App'>
       <div className="container">
-        <div className='row'>
-          <div className='col-md-12 col-sm3'>
-            <nav className='navbar'>
-              <img src={logo} alt="logo" width={80} />
+        <div className="row">
+          <div className="col-lg col-md col-sm">
+            <nav className='navbar navbar-expand-lg'>
+              <div className="container-fluid">
+              <img src={logo} alt="logo" width={100} className='img-fluid'/>
               <Link to="/" className='nav-item'>Home</Link>
               <Link to="/hero" className='nav-item'>About</Link>
               <Link to="/menu" className='nav-item'>Menu</Link>
               <Link to="/reserve" className='nav-item'>Reservation</Link>
               <Link to="/order" className='nav-item'>Order Online</Link>
               <Link to="/login" className='nav-item'>Login</Link>
+              </div>
             </nav>
           </div>
         </div>
